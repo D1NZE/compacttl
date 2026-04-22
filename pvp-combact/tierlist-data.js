1044cd8d22467e399693c26644578375164f2db1
D1NZE <daniel.k.davies@icloud.com>
2026-04-22 22:09:49 +0300

    Update tierlist data

--- pvp-combact/tierlist-data.js
+++ pvp-combact/tierlist-data.js
@@ -1,6 +1,17 @@
 window.TIERLIST_PLAYERS = {
-  "updatedAt": "2026-04-22T19:07:50.990Z",
+  "updatedAt": "2026-04-22T19:09:48.686Z",
   "players": [
+    {
+      "userId": "1164936631908835351",
+      "username": "d1nzee",
+      "displayName": "D1NZE",
+      "ign": "D1NZE",
+      "tier": "HT3",
+      "previousTier": "Migrated",
+      "region": "EU",
+      "source": "Mctier list",
+      "updatedAt": "2026-04-22T19:09:48.673Z"
+    },
     {
       "username": "zxny77",
       "displayName": "Zxny77",
@@ -11,17 +22,6 @@ window.TIERLIST_PLAYERS = {
       "source": "Crystal ranked",
       "updatedAt": "2026-04-22T19:00:01.471Z",
       "userId": "1361510979975905322"
-    },
-    {
-      "userId": "1164936631908835351",
-      "username": "d1nzee",
-      "displayName": "D1NZE",
-      "ign": "D1NZE",
-      "tier": "LT3",
-      "previousTier": "Migrated",
-      "region": "EU",
-      "source": "Lurnss Tierlist",
-      "updatedAt": "2026-04-22T19:07:50.989Z"
     },
     {
       "username": "nolimitblock_28207",
