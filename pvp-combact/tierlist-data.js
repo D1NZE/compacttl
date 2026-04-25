eafb82897b0d3c9b60585cbf6ab8bf0429380b7f
D1NZE <daniel.k.davies@icloud.com>
2026-04-25 16:06:29 +0300

    Update tierlist data

--- pvp-combact/tierlist-data.js
+++ pvp-combact/tierlist-data.js
@@ -1,5 +1,5 @@
 window.TIERLIST_PLAYERS = {
-  "updatedAt": "2026-04-25T11:34:47.078Z",
+  "updatedAt": "2026-04-25T13:06:24.752Z",
   "players": [
     {
       "username": "laxoooo",
@@ -22,17 +22,6 @@ window.TIERLIST_PLAYERS = {
       "updatedAt": "2026-04-24T22:25:58.424Z",
       "displayName": "Zxny77",
       "previousTier": "HT3"
-    },
-    {
-      "ign": "D1NZE",
-      "tier": "HT3",
-      "region": "EU",
-      "source": "Tested",
-      "userId": "1164936631908835351",
-      "username": "d1nzee",
-      "updatedAt": "2026-04-25T11:34:47.075Z",
-      "displayName": "D1NZE",
-      "previousTier": "LT3"
     },
     {
       "ign": "draculealol",
@@ -77,6 +66,17 @@ window.TIERLIST_PLAYERS = {
       "source": "Miragrated",
       "updatedAt": "2026-04-25T10:35:20.585Z",
       "userId": "932713271008579655"
+    },
+    {
+      "ign": "D1NZE",
+      "tier": "LT3",
+      "region": "EU",
+      "source": "Tested",
+      "userId": "1164936631908835351",
+      "username": "d1nzee",
+      "updatedAt": "2026-04-25T13:06:24.749Z",
+      "displayName": "D1NZE",
+      "previousTier": "HT3"
     },
     {
       "ign": "itszvon",
